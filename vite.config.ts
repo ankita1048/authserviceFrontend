4e1e2e397bb25fe0b6d81de2838ebb777039102d
ankita1048 <ankita.ray@antiersolutions.com>
2025-10-01 15:01:23 +0530

    vite config file content changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -15,5 +16,18 @@ export default defineConfig({
       },
       shared: ['react', 'react-dom', 'react-hook-form']
     })
-  ]
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.tsx'), // <-- your main entry file
+      name: 'authMicrofrontend',
+      formats: ['es'], // export as ESM
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-hook-form'],
+    },
+  },
 }); 
